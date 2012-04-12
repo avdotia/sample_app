8639b789edad9a88a2fa2df7bdd0e0764d75358d
Laura <lauraperezfelipe@gmail.com>
2012-04-12 20:45:48 +0100

    Finalizado el capitulo 5. añani un helper method por la variable logo y mejore los tests, los links corresponden a las paginas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def logo
+    @logo = image_tag("/images/logo.png", :alt => "Sample App", :class => "round")
+  end
   # Return a title on a per-page basis.
   def title
     base_title= "Ruby on Rails Tutorial Sample App"
